38baadbe2fe7e44eca596b8e3b418295074a94fe
Roberts Kalnins <rkalnins314@gmail.com>
2019-01-27 12:48:31 -0500

    Cleanup

--- src/main/java/frc/team2767/deepspace/command/TwistService.java
+++ src/main/java/frc/team2767/deepspace/command/TwistService.java
@@ -12,7 +12,7 @@ import org.slf4j.LoggerFactory;
 
 public class TwistService extends InstantCommand {
 
-  private static final DriveSubsystem swerve = Robot.DRIVE;
+  private static final DriveSubsystem DRIVE = Robot.DRIVE;
   private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
   @Override
@@ -22,14 +22,13 @@ public class TwistService extends InstantCommand {
     NetworkTableEntry range = table.getEntry("camera_range");
     double cameraAngle = (double) bearing.getNumber(0.0);
     double cameraRange = (double) range.getNumber(0.0);
-    double cameraX = 13.5; // TODO: Update camera positions
-    double cameraY = 0.0; // TODO: Update camera positions
-    double swerveRotation = swerve.getGyro().getYaw();
+    double cameraX = 13.5;
+    double cameraY = 0.0;
+    double swerveRotation = DRIVE.getGyro().getYaw();
 
     TwistCalculator twistCalculator =
         new TwistCalculator(cameraAngle, cameraRange, cameraX, cameraY, swerveRotation);
 
-    //    logger.debug("range={} bearing={}", range.getDouble(2767), bearing.getDouble(2767));
     logger.debug("range={} heading={}", twistCalculator.getRange(), twistCalculator.getHeading());
 
     double targetYaw = -90.0;
