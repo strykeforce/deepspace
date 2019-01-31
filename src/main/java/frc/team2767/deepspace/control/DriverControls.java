b53b653a865472054bbff07cf2633b833acc3c94
Roberts Kalnins <rkalnins314@gmail.com>
2019-01-31 16:55:45 -0500

    Fix build failure

--- src/main/java/frc/team2767/deepspace/control/DriverControls.java
+++ src/main/java/frc/team2767/deepspace/control/DriverControls.java
@@ -4,10 +4,10 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import edu.wpi.first.wpilibj.command.Command;
 import frc.team2767.deepspace.command.LogCommand;
-import frc.team2767.deepspace.command.TwistService;
+import frc.team2767.deepspace.command.TwistSetup;
+import frc.team2767.deepspace.command.ZeroGyroCommand;
 import frc.team2767.deepspace.command.vision.LightsOffCommand;
 import frc.team2767.deepspace.command.vision.LightsOnCommand;
-import frc.team2767.deepspace.command.ZeroGyroCommand;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -32,7 +32,7 @@ public class DriverControls {
 
     new JoystickButton(joystick, Button.HAMBURGER.id).whenPressed(log(Button.HAMBURGER));
     new JoystickButton(joystick, Button.X.id).whenPressed(log(Button.X));
-    new JoystickButton(joystick, Button.UP.id).whenPressed(new TwistService());
+    new JoystickButton(joystick, Button.UP.id).whenPressed(new TwistSetup());
     new JoystickButton(joystick, Button.DOWN.id).whenPressed(log(Button.DOWN));
 
     // Trim Switches
