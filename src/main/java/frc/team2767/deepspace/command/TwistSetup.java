4601add8f7ca55328c5ee490bdf12a40aa0c3161
Roberts Kalnins <rkalnins314@gmail.com>
2019-01-31 16:55:45 -0500

    Fix build failure

--- src/main/java/frc/team2767/deepspace/command/TwistSetup.java
+++ src/main/java/frc/team2767/deepspace/command/TwistSetup.java
@@ -3,19 +3,19 @@ package frc.team2767.deepspace.command;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.command.InstantCommand;
 import frc.team2767.deepspace.Robot;
 import frc.team2767.deepspace.subsystem.DriveSubsystem;
 import frc.team2767.deepspace.util.TwistCalculator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class TwistService {
+public class TwistSetup extends InstantCommand {
 
   private static final DriveSubsystem DRIVE = Robot.DriveSubsystem;
   private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-
-  public TwistService() {
+  public void initialize() {
     NetworkTable table = NetworkTableInstance.getDefault().getTable("Pyeye");
     NetworkTableEntry bearing = table.getEntry("camera_bearing");
     NetworkTableEntry range = table.getEntry("camera_range");
@@ -26,7 +26,7 @@ public class TwistService {
     double swerveRotation = DRIVE.getGyro().getYaw();
 
     TwistCalculator twistCalculator =
-            new TwistCalculator(cameraAngle, cameraRange, cameraX, cameraY, swerveRotation);
+        new TwistCalculator(cameraAngle, cameraRange, cameraX, cameraY, swerveRotation);
 
     logger.debug("range={} heading={}", twistCalculator.getRange(), twistCalculator.getHeading());
 
@@ -37,5 +37,4 @@ public class TwistService {
     //        (int) (DriveSubsystem.TICKS_PER_INCH * twistCalculator.getRange()),
     //        targetYaw);
   }
-
 }
