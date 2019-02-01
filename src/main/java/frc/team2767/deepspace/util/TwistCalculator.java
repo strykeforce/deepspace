519f739be655e36bab0466aaa2d9371863eedced
Roberts Kalnins <rkalnins314@gmail.com>
2019-02-01 17:25:50 -0500

    Remove heading adjustment

--- src/main/java/frc/team2767/deepspace/util/TwistCalculator.java
+++ src/main/java/frc/team2767/deepspace/util/TwistCalculator.java
@@ -6,20 +6,37 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TwistCalculator {
+
+  private final double distanceSafetyAdjustment = 10.0;
   private final DriveSubsystem DRIVE = Robot.DriveSubsystem;
   private final Logger logger = LoggerFactory.getLogger(this.getClass());
   private double deltaX;
   private double deltaY;
   private double cameraAngle;
   private double cameraRange;
+  private double cameraX;
+  private double cameraY;
   private double cameraPositionBearing;
+  private double swerveRotation;
 
-  public TwistCalculator(double cameraAngle, double cameraRange, double cameraPositionBearing) {
+  public TwistCalculator(
+      double cameraAngle,
+      double cameraRange,
+      double cameraX,
+      double cameraY,
+      double cameraPositionBearing,
+      double swerveRotation) {
 
-    computeNew(cameraAngle, cameraRange, cameraPositionBearing);
+    computeNew(cameraAngle, cameraRange, cameraX, cameraY, cameraPositionBearing, swerveRotation);
   }
 
-  private void computeNew(double cameraAngle, double cameraRange, double cameraPositionBearing) {
+  private void computeNew(
+      double cameraAngle,
+      double cameraRange,
+      double cameraX,
+      double cameraY,
+      double cameraPositionBearing,
+      double swerveRotation) {
 
     deltaX = 0.0;
     deltaY = 0.0;
@@ -28,14 +45,34 @@ public class TwistCalculator {
     double transferSlope = 1.2449;
     double transferIntercept = -4.3949;
     this.cameraRange = transferSlope * cameraRange + transferIntercept;
+    this.cameraX = cameraX;
+    this.cameraY = cameraY;
     this.cameraPositionBearing = cameraPositionBearing;
+    this.swerveRotation = swerveRotation;
 
     targetToCamera();
+    cameraToRobot();
+    robotToSwerve();
   }
 
   private void targetToCamera() {
     deltaX = (cameraRange * Math.cos(Math.toRadians(cameraAngle)));
     deltaY = (cameraRange * Math.sin(Math.toRadians(cameraAngle)));
+  }
+
+  private void cameraToRobot() {
+    deltaX += cameraX;
+    deltaY += cameraY;
+  }
+
+  private void robotToSwerve() {
+    double dXtemp = deltaX;
+    deltaX =
+        deltaX * Math.cos(Math.toRadians(swerveRotation))
+            - deltaY * Math.sin(Math.toRadians(swerveRotation));
+    deltaY =
+        deltaY * Math.cos(Math.toRadians(swerveRotation))
+            + dXtemp * Math.sin(Math.toRadians(swerveRotation));
   }
 
   /** @return twist heading */
@@ -45,6 +82,6 @@ public class TwistCalculator {
 
   /** @return twist range */
   public double getRange() {
-    return Math.hypot(deltaX, deltaY);
+    return Math.hypot(deltaX, deltaY) - distanceSafetyAdjustment;
   }
 }
