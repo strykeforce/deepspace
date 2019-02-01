f4df04f1c6a61342e35ab275a537c3e4fcf194fa
Roberts Kalnins <rkalnins314@gmail.com>
2019-02-01 17:25:50 -0500

    Remove heading adjustment

--- src/main/java/frc/team2767/deepspace/control/SmartDashboardControls.java
+++ src/main/java/frc/team2767/deepspace/control/SmartDashboardControls.java
@@ -1,14 +1,10 @@
 package frc.team2767.deepspace.control;
 
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import frc.team2767.deepspace.command.vision.LightsOffCommand;
-import frc.team2767.deepspace.command.vision.LightsOnCommand;
-
 // FIXME
 public class SmartDashboardControls {
 
   public SmartDashboardControls() {
-    SmartDashboard.putData("Lights On", new LightsOnCommand());
-    SmartDashboard.putData("Lights Off", new LightsOffCommand());
+    //    SmartDashboard.putData("Lights On", new LightsOnCommand());
+    //    SmartDashboard.putData("Lights Off", new LightsOffCommand());
   }
 }
