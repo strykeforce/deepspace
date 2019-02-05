b616391f160fdc6391fe3f9f33f74bbae8a3d923
rabsamu <30671368+rabsamu@users.noreply.github.com>
2019-02-05 18:29:07 -0500

    Change biscuit mode to position

---
+++ src/main/java/frc/team2767/deepspace/command/biscuit/BiscuitPosition.java
@@ -0,0 +1,25 @@
+package frc.team2767.deepspace.command.biscuit;
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.team2767.deepspace.Robot;
+import frc.team2767.deepspace.subsystem.BiscuitSubsystem;
+
+public class BiscuitPosition extends Command {
+    BiscuitSubsystem biscuitSubsystem = Robot.BiscuitSubsystem;
+    BiscuitSubsystem.Position position;
+
+    public BiscuitPosition(BiscuitSubsystem.Position position){
+        this.position = position;
+        requires(biscuitSubsystem);
+    }
+
+    @Override
+    protected void initialize(){
+        biscuitSubsystem.startMotion(position);
+    }
+
+    @Override
+    protected boolean isFinished(){
+        return biscuitSubsystem.positionReached(position);
+    }
+}
--- src/main/java/frc/team2767/deepspace/subsystem/BiscuitSubsystem.java
+++ src/main/java/frc/team2767/deepspace/subsystem/BiscuitSubsystem.java
@@ -3,78 +3,95 @@ package frc.team2767.deepspace.subsystem;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class BiscuitSubsystem extends Subsystem {
 
-    final int CLOSE_ENOUGH = 0;
-    final int UP_POS = 0;
-    final int DOWN_POS = 0;
-    final int LEFT_POS = 0;
-    final int RIGHT_POS = 0;
-    final int BISCUIT_ID = 40;
+    private final int CLOSE_ENOUGH = 0;//FIXME
+    private final int UP_POS = 0;
+    private final int DOWN_POS = 0;//FIXME
+    private final int LEFT_POS = 0;//FIXME
+    private final int RIGHT_POS = 0;//FIXME
+    private final int BISCUIT_ID = 40;
+    private final int NUM_ROTATIONS = 2;//FIXME
+    private final int TICKS_PER_REV = 12300;
+    private final int LOW_ENCODER_LIMIT = -24600;//FIXME
+    private final int HIGH_ENCODER_LIMIT = 24600;//FIXME
 
     TalonSRX biscuit = new TalonSRX(BISCUIT_ID);
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Override
     protected void initDefaultCommand() {}
 
-    public void zero() {}
-
-    public void pickDirection (Position position) {
-        switch (position){
-            case UP:
-                if (biscuit.getSelectedSensorPosition() - UP_POS > 0){
-                    run(-.20);
-                }
-                break;
-            case DOWN:
-                break;
-            case FORWARD:
-                break;
-            case BACK:
-                break;
-
-        }
-
+    public void zero(){
+        biscuit.setSelectedSensorPosition(0);
     }
 
-    public void run (double power){
+    public int getPosition(){
+        if (biscuit.getSelectedSensorPosition() >= 0){
+            return biscuit.getSelectedSensorPosition() % TICKS_PER_REV;
+        } else {
+            return biscuit.getSelectedSensorPosition() % -TICKS_PER_REV;
+        }
+    }
+
+    int getEncoderValue(Position position){
+        switch(position){
+            case UP:
+                return UP_POS;
+            case DOWN:
+                return DOWN_POS;
+            case LEFT:
+                return LEFT_POS;
+            case RIGHT:
+                return RIGHT_POS;
+            default:
+                logger.warn("Invalid biscuit position");
+                return 0;
+        }
+    }
+
+    int findNearest (int basePosition){
+        int nearest = 0;
+        for (int k = -NUM_ROTATIONS; k <= NUM_ROTATIONS; k++){
+            int loopedPosition = basePosition + k * TICKS_PER_REV;
+            if (Math.abs(biscuit.getSelectedSensorPosition() - loopedPosition) < nearest &&
+                    LOW_ENCODER_LIMIT < loopedPosition &&
+                    HIGH_ENCODER_LIMIT > loopedPosition){
+                nearest = loopedPosition;
+            }
+        }
+        return nearest;
+    }
+
+    public void setPosition(Position position){
+        int target = findNearest(getEncoderValue(position));
+        biscuit.set(ControlMode.Position, target);
+    }
+
+    void runOpenLoop(double power){
         biscuit.set(ControlMode.PercentOutput, power);
     }
 
-    public enum Position {
+    public boolean positionReached (Position position){
+        if (Math.abs(biscuit.getSelectedSensorPosition() - getEncoderValue(position)) < CLOSE_ENOUGH){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void stop(){
+        biscuit.set(ControlMode.PercentOutput, 0);
+    }
+
+    public enum Position{
         UP,
         DOWN,
-        FORWARD,
-        BACK
+        LEFT,
+        RIGHT
     }
-/*
-    need to know:
-        max revs
-        encoder vals of positions
-        good speed
-        good enough
-    settings:
-        closeEnough
-        upPos
-        downPos
-        backPos
-        forwardPos
-    Objects: 1 motor 40, 1 absolute encoder, enum position
-    Methods:
-        check encoder is good
-        zero/check zero
-        Start(pos)-runs sequence of pick direction and run
-        pickDirection(position)-decide fastest direction of rotation, use report to decide if safe
-        forward-start motor in positive direction
-        backwards-start motor in negative direction
-        report-return the current encoder pos
-        position reached (enum position)- current-goal<good enough
-        stop
-    enum Position
-        up
-        forward
-        backwards
-        down
-*/
 }
