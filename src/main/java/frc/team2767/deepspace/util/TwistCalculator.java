14ea451712f08a020de20b1b898ee310c997526a
Roberts Kalnins <rkalnins314@gmail.com>
2019-02-11 22:33:03 -0500

    Add tests for twist calculator, ParameterizedTests still broken

--- src/main/java/frc/team2767/deepspace/util/TwistCalculator.java
+++ src/main/java/frc/team2767/deepspace/util/TwistCalculator.java
@@ -29,81 +29,40 @@ public class TwistCalculator {
     double transferSlope = 1.2449;
     double transferIntercept = -4.3949;
     double range = transferSlope * cameraRange + transferIntercept;
-    //    double range = cameraRange;
     this.cameraX = cameraX;
     this.cameraY = cameraY;
-
-    logger.debug(
-        "9 * Math.sin(Math.toRadians(targetYaw)) = -9.0 = {}",
-        9.0 * Math.sin(Math.toRadians(-90.0)));
 
     double currentX =
         cameraX * Math.cos(Math.toRadians(swerveRotation))
             - cameraY * Math.sin(Math.toRadians(swerveRotation));
 
-    logger.debug(
-        "cX = {} = {} - {}",
-        currentX,
-        cameraX * Math.cos(Math.toRadians(swerveRotation)),
-        cameraY * Math.sin(Math.toRadians(swerveRotation)));
-
     double currentY =
         cameraY * Math.cos(Math.toRadians(swerveRotation))
             + cameraX * Math.sin(Math.toRadians(swerveRotation));
-
-    logger.debug(
-        "cY = {} = {} + {}",
-        currentY,
-        cameraY * Math.cos(Math.toRadians(swerveRotation)),
-        cameraX * Math.sin(Math.toRadians(swerveRotation)));
 
     double finalX =
         cameraX * Math.cos(Math.toRadians(targetYaw))
             - cameraY * Math.sin(Math.toRadians(targetYaw));
 
-    logger.debug(
-        "finalX = {} = {} - {}",
-        finalX,
-        cameraX * Math.cos(Math.toRadians(targetYaw)),
-        cameraY * Math.sin(Math.toRadians(targetYaw)));
-
     double finalY =
         cameraY * Math.cos(Math.toRadians(targetYaw))
             + cameraX * Math.sin(Math.toRadians(targetYaw));
 
-    logger.debug(
-        "finalY = {} = {} + {}",
-        finalY,
-        cameraY * Math.cos(Math.toRadians(targetYaw)),
-        cameraX * Math.sin(Math.toRadians(targetYaw)));
-
-    //    logger.debug("cX={} cY={}", currentX, currentY);
-    //    logger.debug("fX={} fY={}", finalX, finalY);
-
     deltaX = finalX - currentX;
     deltaY = finalY - currentY;
 
-    logger.debug("dX={} dY={}", deltaX, deltaY);
-
     double initialHeading = cameraPositionBearing + DRIVE.getGyro().getAngle() + cameraAngle;
-
-    logger.debug("original heading={}", initialHeading);
 
     double headingX = range * Math.cos(Math.toRadians(initialHeading));
     double headingY = range * Math.sin(Math.toRadians(initialHeading));
 
-    logger.debug("headingX={} headingY={}", headingX, headingY);
-
     double xCorrected = headingX - deltaX;
     double yCorrected = headingY - deltaY;
 
-    logger.debug("xCorrected={} yCorrected={}", xCorrected, yCorrected);
-
     finalHeading = Math.toDegrees(Math.atan2(yCorrected, xCorrected));
-
     finalRange = Math.hypot(xCorrected, yCorrected);
 
-    logger.debug("finalHeading = {} finalRange = {}", finalHeading, finalRange);
+    logger.debug("Driving range of {} at {}", finalRange, finalHeading);
   }
 
   /** @return twist heading */
---
+++ src/test/java/frc/team2767/deepspace/util/TwistCalculatorTest.java
@@ -0,0 +1,64 @@
+package frc.team2767.deepspace.util;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.assertj.core.data.Percentage;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+class TwistCalculatorTest {
+
+  @Disabled
+  @ParameterizedTest
+  @CsvFileSource(resources = "inputs.csv", numLinesToSkip = 1)
+  void getHeading(
+      double cameraAngle,
+      double cameraRange,
+      double cameraX,
+      double cameraY,
+      double cameraPositionBearing,
+      double swerveRotation,
+      double targetYaw,
+      double expectedRange) {
+
+    TwistCalculator twistCalculator =
+        new TwistCalculator(
+            cameraAngle,
+            cameraRange,
+            cameraX,
+            cameraY,
+            cameraPositionBearing,
+            swerveRotation,
+            targetYaw);
+
+    assertThat(twistCalculator.getHeading())
+        .isCloseTo(expectedRange, Percentage.withPercentage(0.1));
+  }
+
+  @Disabled
+  @ParameterizedTest
+  @CsvFileSource(resources = "inputs.csv", numLinesToSkip = 1)
+  void getRange(
+      double cameraAngle,
+      double cameraRange,
+      double cameraX,
+      double cameraY,
+      double cameraPositionBearing,
+      double swerveRotation,
+      double targetYaw,
+      double expectedRange) {
+
+    TwistCalculator twistCalculator =
+        new TwistCalculator(
+            cameraAngle,
+            cameraRange,
+            cameraX,
+            cameraY,
+            cameraPositionBearing,
+            swerveRotation,
+            targetYaw);
+
+    assertThat(twistCalculator.getRange()).isCloseTo(expectedRange, Percentage.withPercentage(0.1));
+  }
+}
