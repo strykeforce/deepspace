18b831e5ea02924f94149fde8575f16a366de2da
Jeff Hutchison <jeff@jeffhutchison.com>
2019-02-12 19:18:45 -0500

    Add skeletal safety subsystem

---
+++ src/main/java/frc/team2767/deepspace/subsystem/safety/IntakePosition.java
@@ -0,0 +1,18 @@
+package frc.team2767.deepspace.subsystem.safety;
+
+enum IntakePosition {
+  INTAKE_INTAKE(0, 0),
+  INTAKE_STOW(0, 0);
+
+  public final int forwardLimit;
+  public final int reverseLimit;
+
+  IntakePosition(int forwardLimit, int reverseLimit) {
+    this.forwardLimit = forwardLimit;
+    this.reverseLimit = reverseLimit;
+  }
+
+  public static IntakePosition of(int position) {
+    return INTAKE_STOW;
+  }
+}
