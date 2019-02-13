9ae0686b53e60b280c98ef5c3ecbc70783a82802
Roberts Kalnins <rkalnins314@gmail.com>
2019-02-12 19:49:54 -0500

    Add shuffleboard

--- src/main/java/frc/team2767/deepspace/control/SmartDashboardControls.java
+++ src/main/java/frc/team2767/deepspace/control/SmartDashboardControls.java
@@ -1,10 +1,43 @@
 package frc.team2767.deepspace.control;
 
-// FIXME
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.team2767.deepspace.command.elevator.ElevatorZeroCommand;
+import frc.team2767.deepspace.command.intake.IntakeInCommand;
+import frc.team2767.deepspace.command.intake.IntakeOutCommand;
+import frc.team2767.deepspace.command.vision.LightsOffCommand;
+import frc.team2767.deepspace.command.vision.LightsOnCommand;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class SmartDashboardControls {
 
+  private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
   public SmartDashboardControls() {
-    //    SmartDashboard.putData("Lights On", new LightsOnCommand());
-    //    SmartDashboard.putData("Lights Off", new LightsOffCommand());
+    addMatchCommands();
+    addPitCommands();
+    addTestCommands();
+    addVisionCommands();
+  }
+
+  private void addMatchCommands() {
+    logger.debug("creating match commands");
+    SmartDashboard.putData("Game/IntakeIn", new IntakeInCommand());
+    SmartDashboard.putData("Game/IntakeOut", new IntakeOutCommand());
+  }
+
+  private void addPitCommands() {
+    addTestCommands();
+    SmartDashboard.putData("Test/ElevatorStop", new ElevatorZeroCommand());
+  }
+
+  private void addTestCommands() {
+    logger.debug("creating test commands");
+  }
+
+  private void addVisionCommands() {
+    logger.debug("creating vision commands");
+    SmartDashboard.putData("Pit/LightsOn", new LightsOnCommand());
+    SmartDashboard.putData("Pit/LightsOff", new LightsOffCommand());
   }
 }
