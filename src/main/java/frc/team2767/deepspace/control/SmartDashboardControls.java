7c83578a2988c0722e3cb6a5720ee437989b6085
Roberts Kalnins <rkalnins314@gmail.com>
2019-02-15 19:46:26 -0500

    Add smartdashboard commands

--- src/main/java/frc/team2767/deepspace/control/SmartDashboardControls.java
+++ src/main/java/frc/team2767/deepspace/control/SmartDashboardControls.java
@@ -1,11 +1,17 @@
 package frc.team2767.deepspace.control;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.team2767.deepspace.command.elevator.ElevatorOpenLoopDownCommand;
+import frc.team2767.deepspace.command.elevator.ElevatorOpenLoopUpCommand;
+import frc.team2767.deepspace.command.elevator.ElevatorStopCommand;
 import frc.team2767.deepspace.command.elevator.ElevatorZeroCommand;
 import frc.team2767.deepspace.command.intake.IntakeInCommand;
 import frc.team2767.deepspace.command.intake.IntakeOutCommand;
+import frc.team2767.deepspace.command.intake.IntakeStopCommand;
+import frc.team2767.deepspace.command.vacuum.PressureSetCommand;
 import frc.team2767.deepspace.command.vision.LightsOffCommand;
 import frc.team2767.deepspace.command.vision.LightsOnCommand;
+import frc.team2767.deepspace.subsystem.VacuumSubsystem;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -28,10 +34,14 @@ public class SmartDashboardControls {
 
   private void addPitCommands() {
     addTestCommands();
-    SmartDashboard.putData("Test/ElevatorStop", new ElevatorZeroCommand());
+    addIntakeCommands();
+    addElevatorComamnds();
+    addVacuumCommands();
+    SmartDashboard.putData("Pit/ElevatorZero", new ElevatorZeroCommand());
   }
 
   private void addTestCommands() {
+
     logger.debug("creating test commands");
   }
 
@@ -39,5 +49,26 @@ public class SmartDashboardControls {
     logger.debug("creating vision commands");
     SmartDashboard.putData("Pit/LightsOn", new LightsOnCommand());
     SmartDashboard.putData("Pit/LightsOff", new LightsOffCommand());
+  }
+
+  private void addIntakeCommands() {
+    SmartDashboard.putData("Pit/IntakeOut", new IntakeOutCommand());
+    SmartDashboard.putData("Pit/IntakeStop", new IntakeStopCommand());
+    SmartDashboard.putData("Pit/IntakeIn", new IntakeInCommand());
+  }
+
+  private void addElevatorComamnds() {
+    SmartDashboard.putData("Pit/ElevatorUp", new ElevatorOpenLoopUpCommand());
+    SmartDashboard.putData("Pit/ElevatorStop", new ElevatorStopCommand());
+    SmartDashboard.putData("Pit/ElevatorDown", new ElevatorOpenLoopDownCommand());
+  }
+
+  private void addVacuumCommands() {
+    SmartDashboard.putData(
+        "Pit/VacuumHatch", new PressureSetCommand(VacuumSubsystem.VacuumPressure.HATCH));
+    SmartDashboard.putData(
+        "Pit/Vacuum/Ball", new PressureSetCommand(VacuumSubsystem.VacuumPressure.BALL));
+    SmartDashboard.putData(
+        "Pit/Vacuum/Climb", new PressureSetCommand(VacuumSubsystem.VacuumPressure.CLIMB));
   }
 }
