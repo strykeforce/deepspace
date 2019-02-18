e4e776123c6fc743347c75e37a87e9d97ae4175d
mwitcpalek <35316453+mwitcpalek@users.noreply.github.com>
2019-02-17 20:19:57 -0500

    Remove Xbox controls from Controls class

--- src/main/java/frc/team2767/deepspace/control/Controls.java
+++ src/main/java/frc/team2767/deepspace/control/Controls.java
@@ -4,7 +4,7 @@ public class Controls {
 
   private final DriverControls driverControls = new DriverControls(0);
   private final GameControls gameControls = new GameControls(1);
-  private final XboxControls xboxControls = new XboxControls(2);
+  //private final XboxControls xboxControls = new XboxControls(2);
   private final SmartDashboardControls smartDashboardControls = new SmartDashboardControls();
 
   public Controls() {
@@ -19,9 +19,9 @@ public class Controls {
     return gameControls;
   }
 
-  public XboxControls getXboxControls() {
+  /*public XboxControls getXboxControls() {
     return xboxControls;
-  }
+  }*/
 
   public SmartDashboardControls getSmartDashboardControls() {
     return smartDashboardControls;
