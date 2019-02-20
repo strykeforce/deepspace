030ce83ba5f550472bb1795f0f6e09311028be63
Roberts Kalnins <rkalnins314@gmail.com>
2019-02-20 16:58:05 -0500

    Remove unnecessary id in elevator level

--- src/main/java/frc/team2767/deepspace/subsystem/ElevatorLevel.java
+++ src/main/java/frc/team2767/deepspace/subsystem/ElevatorLevel.java
@@ -1,14 +1,8 @@
 package frc.team2767.deepspace.subsystem;
 
 public enum ElevatorLevel {
-  ONE(1),
-  TWO(2),
-  THREE(3),
-  NOTSET(-1);
-
-  private int id;
-
-  ElevatorLevel(int id) {
-    this.id = id;
-  }
+  ONE,
+  TWO,
+  THREE,
+  NOTSET
 }
