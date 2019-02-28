b82f58105c398bc21696316756708e68a61f3da9
Roberts Kalnins <rkalnins314@gmail.com>
2019-02-28 16:42:23 -0500

    Resolve #125

--- src/main/java/frc/team2767/deepspace/command/sequences/CargoGroundPickupCommandGroup.java
+++ src/main/java/frc/team2767/deepspace/command/sequences/CargoGroundPickupCommandGroup.java
@@ -2,7 +2,6 @@ package frc.team2767.deepspace.command.sequences;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import frc.team2767.deepspace.command.biscuit.BiscuitSetPositionCommand;
-import frc.team2767.deepspace.command.coconut.CoconutOpenCommand;
 import frc.team2767.deepspace.command.elevator.ElevatorSetPositionCommand;
 import frc.team2767.deepspace.command.intake.IntakePositionCommand;
 import frc.team2767.deepspace.command.intake.RollerInCommand;
@@ -15,7 +14,6 @@ public class CargoGroundPickupCommandGroup extends CommandGroup {
     addSequential(new ElevatorSetPositionCommand(22.0));
     addSequential(new BiscuitSetPositionCommand(BiscuitSubsystem.kDownRightPositionDeg));
     addSequential(new RollerInCommand());
-    addParallel(new CoconutOpenCommand());
     addSequential(new IntakePositionCommand(IntakeSubsystem.kLoadPositionDeg));
   }
 }
