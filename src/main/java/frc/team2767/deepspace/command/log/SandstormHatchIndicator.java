78eb3d425cb87d1bf1997c0c5c75eb309f44cda8
rl507 <32242704+rl507@users.noreply.github.com>
2019-03-02 14:49:25 -0500

    Sandstorm Sequence (#148)

---
+++ src/main/java/frc/team2767/deepspace/command/log/SandstormHatchIndicator.java
@@ -0,0 +1,17 @@
+package frc.team2767.deepspace.command.log;
+
+import edu.wpi.first.wpilibj.command.InstantCommand;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class SandstormHatchIndicator extends InstantCommand {
+  private boolean state;
+
+  public SandstormHatchIndicator(boolean current) {
+    state = current;
+  }
+
+  @Override
+  protected void initialize() {
+    SmartDashboard.putBoolean("Hatch/SandstormPickUp", state);
+  }
+}
