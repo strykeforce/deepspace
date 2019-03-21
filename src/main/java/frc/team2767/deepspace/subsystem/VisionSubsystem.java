59470e5d8f5357ac680efe5514c931c4b064a11b
Roberts Kalnins <rkalnins314@gmail.com>
2019-03-20 21:52:05 -0400

    Finish tuning vision.

--- src/main/java/frc/team2767/deepspace/subsystem/VisionSubsystem.java
+++ src/main/java/frc/team2767/deepspace/subsystem/VisionSubsystem.java
@@ -22,9 +22,9 @@ public class VisionSubsystem extends Subsystem {
   private static final double CAMERA_Y_LEFT = -13.5;
   private static final double CAMERA_Y_RIGHT = 13.5;
   private static final double GLUE_CORRECTION_FACTOR_RIGHT = 2.0;
-  private static final double GLUE_CORRECTION_FACTOR_LEFT = 0.0;
-  private static final double CAMERA_DEGREES_PER_PIXEL_ADJUSTMENT_RIGHT = 1.0; // 1.0 is zero value
-  private static final double CAMERA_DEGREES_PER_PIXEL_ADJUSTMENT_LEFT = 1.0; // 1.0 is zero value
+  private static final double GLUE_CORRECTION_FACTOR_LEFT = 2.0;
+  private static final double CAMERA_DEGREES_PER_PIXEL_ADJUSTMENT_RIGHT = 0.7; // 1.0 is zero value
+  private static final double CAMERA_DEGREES_PER_PIXEL_ADJUSTMENT_LEFT = 0.7; // 1.0 is zero value
   private static final double CAMERA_POSITION_BEARING_LEFT = -90.0;
   private static final double CAMERA_POSITION_BEARING_RIGHT = 90.0;
   private final Logger logger = LoggerFactory.getLogger(this.getClass());
