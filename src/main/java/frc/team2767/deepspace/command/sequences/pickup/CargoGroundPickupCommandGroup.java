6ec5b96eabd74299bb5eebd9e1d690d42b289327
Roberts Kalnins <rkalnins314@gmail.com>
2019-03-30 22:21:30 -0400

    Update elevator height

--- src/main/java/frc/team2767/deepspace/command/sequences/pickup/CargoGroundPickupCommandGroup.java
+++ src/main/java/frc/team2767/deepspace/command/sequences/pickup/CargoGroundPickupCommandGroup.java
@@ -22,7 +22,7 @@ public class CargoGroundPickupCommandGroup extends CommandGroup {
             addParallel(new BiscuitSetPositionCommand(BiscuitSubsystem.kDownPosition));
           }
         });
-    addSequential(new ElevatorSetPositionCommand(20.25));
+    addSequential(new ElevatorSetPositionCommand(21.25));
     addSequential(new LogCommand("END CARGO GROUND PICKUP"));
   }
 }
