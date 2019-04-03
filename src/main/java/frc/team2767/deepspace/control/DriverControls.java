a60a35f248700bcacd2947f67d0e8260f54bd5a4
mwitcpalek <35316453+mwitcpalek@users.noreply.github.com>
2019-04-02 22:33:40 -0400

    Automate Cargo Pickup from Floor

--- src/main/java/frc/team2767/deepspace/control/DriverControls.java
+++ src/main/java/frc/team2767/deepspace/control/DriverControls.java
@@ -17,8 +17,7 @@ import frc.team2767.deepspace.command.intake.ShoulderStopCommand;
 import frc.team2767.deepspace.command.log.LogCommand;
 import frc.team2767.deepspace.command.log.SafetyLogDumpCommand;
 import frc.team2767.deepspace.command.sequences.StowAllCommandGroup;
-import frc.team2767.deepspace.command.sequences.pickup.CargoGroundPickupCommandGroup;
-import frc.team2767.deepspace.command.sequences.pickup.CoconutPickupCommandGroup;
+import frc.team2767.deepspace.command.sequences.pickup.AutoCargoPickupCommandGroup;
 import frc.team2767.deepspace.command.teleop.InterruptCommand;
 import frc.team2767.deepspace.command.vacuum.SetSolenoidStatesCommand;
 import frc.team2767.deepspace.subsystem.ClimbSubsystem;
@@ -39,9 +38,9 @@ public class DriverControls {
 
     // intake pickup
     new JoystickButton(joystick, Shoulder.RIGHT_DOWN.id)
-        .whenPressed(new CargoGroundPickupCommandGroup());
-    new JoystickButton(joystick, Shoulder.RIGHT_DOWN.id)
-        .whenReleased(new CoconutPickupCommandGroup());
+        .whenPressed(new AutoCargoPickupCommandGroup());
+    // new JoystickButton(joystick, Shoulder.RIGHT_DOWN.id)
+    //    .whenReleased(new CoconutPickupCommandGroup());
 
     new JoystickButton(joystick, Button.X.id).whenPressed(new StowAllCommandGroup());
 
