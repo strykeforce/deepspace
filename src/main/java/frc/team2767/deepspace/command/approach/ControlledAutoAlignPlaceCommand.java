47c66f8f6e6190e7a8cb7d85d32820d220888d3f
rabsamu <30671368+rabsamu@users.noreply.github.com>
2019-06-06 19:04:21 -0400

    Add gyro logic for left side placement

---
+++ src/main/java/frc/team2767/deepspace/command/approach/ControlledAutoAlignPlaceCommand.java
@@ -0,0 +1,109 @@
+package frc.team2767.deepspace.command.approach;
+
+import static frc.team2767.deepspace.subsystem.FieldDirection.LEFT;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.team2767.deepspace.Robot;
+import frc.team2767.deepspace.subsystem.DriveSubsystem;
+import frc.team2767.deepspace.subsystem.VisionSubsystem;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.strykeforce.thirdcoast.swerve.SwerveDrive;
+import org.strykeforce.thirdcoast.util.RateLimit;
+
+public class ControlledAutoAlignPlaceCommand extends Command {
+  private static final double kP_STRAFE = 0.07; // 0.11
+  private static final double kP_YAW = 0.01; // 0.00625 tuning for NT method, 0.01 pyeye
+  private static final double MAX_YAW = 0.3;
+  private static final double goodEnoughYaw = 1.5;
+  private static final double MIN_RANGE = 35.0;
+  private static final double FORWARD_OUTPUT = 0.30;
+
+  private static final DriveSubsystem DRIVE = Robot.DRIVE;
+  private static final VisionSubsystem VISION = Robot.VISION;
+  private final Logger logger = LoggerFactory.getLogger(this.getClass());
+  private double range;
+  private double forward;
+  private double strafeError;
+  private double yawError;
+  private double strafeCorrection;
+  private double gyroAngle;
+  private double targetYaw;
+  private boolean isGood = false;
+  private RateLimit strafeRateLimit;
+
+  public ControlledAutoAlignPlaceCommand() {
+    requires(DRIVE);
+
+    strafeRateLimit = new RateLimit(0.05); // 0.015
+  }
+
+  @Override
+  protected void initialize() {
+    gyroAngle = Math.IEEEremainder(DRIVE.getGyro().getAngle(), 360.0);
+
+    // given left side of robot is placing:
+    if (gyroAngle > -90 && gyroAngle < 45) {
+      targetYaw = 0;
+    } else if (gyroAngle >= 45 && gyroAngle <= 135) {
+      targetYaw = 90;
+    } else {
+      targetYaw = 180;
+    }
+
+    // DRIVE.setGyroOffset(gyroOffset);
+    SmartDashboard.putBoolean("Game/haveHatch", true);
+    logger.info("Begin Vision Auto Align Place");
+    DRIVE.setDriveMode(SwerveDrive.DriveMode.CLOSED_LOOP);
+    strafeCorrection = VISION.getStrafeCorrection();
+    if (VISION.direction == LEFT) {
+      targetYaw = 90.0;
+    } else {
+      targetYaw = -90.0;
+    }
+    DRIVE.setTargetYaw(targetYaw);
+    forward = FORWARD_OUTPUT;
+    logger.debug("forward = {}", forward);
+    logger.info("target yaw: {}", targetYaw);
+  }
+
+  @SuppressWarnings("Duplicates")
+  @Override
+  protected void execute() {
+    // Pyeye Method:
+    VISION.queryPyeye(); // gets corrected heading and range from NT
+    range = VISION.getRawRange();
+    isGood = range >= 0; // check if range is good (we have a target), not -1
+
+    // Calculate Yaw Term based on gyro
+    yawError = targetYaw - Math.IEEEremainder(DRIVE.getGyro().getAngle(), 360.0);
+    DRIVE.setYawError(yawError);
+    double yaw = kP_YAW * yawError;
+    if (yaw > MAX_YAW) yaw = MAX_YAW;
+    if (yaw < -MAX_YAW) yaw = -MAX_YAW;
+
+    // Determine if actual yaw is close enough to target
+    boolean onTarget = Math.abs(yawError) <= goodEnoughYaw;
+
+    double strafe;
+    strafeError = Math.sin(Math.toRadians(VISION.getCorrectedBearing())) * range - strafeCorrection;
+    VISION.setStrafeError(strafeError);
+    logger.info("strafe error = {}", strafeError);
+    // Only take over strafe control if pyeye has a target and the robot is straight to the field
+    if (isGood && onTarget) strafe = strafeError * kP_STRAFE * forward;
+    else strafe = 0;
+
+    DRIVE.drive(forward, strafeRateLimit.apply(strafe), yaw);
+  }
+
+  @Override
+  protected boolean isFinished() {
+    return (range <= MIN_RANGE && isGood);
+  }
+
+  @Override
+  protected void end() {
+    logger.info("End Auto Align Place Vision");
+  }
+}
---
+++ src/main/java/frc/team2767/deepspace/command/approach/sequences/ControlledHatchPlaceCommandGroup.java
@@ -0,0 +1,41 @@
+package frc.team2767.deepspace.command.approach.sequences;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import frc.team2767.deepspace.command.approach.HoldHeadingUntilCompressionCommand;
+import frc.team2767.deepspace.command.approach.ControlledAutoAlignPlaceCommand;
+import frc.team2767.deepspace.command.biscuit.BiscuitExecutePlanCommand;
+import frc.team2767.deepspace.command.biscuit.BiscuitPositionAboveCameraCommand;
+import frc.team2767.deepspace.command.elevator.ElevatorSetPositionCommand;
+import frc.team2767.deepspace.command.log.LogCommand;
+import frc.team2767.deepspace.command.states.SetActionCommand;
+import frc.team2767.deepspace.command.states.SetGamePieceCommand;
+import frc.team2767.deepspace.command.states.SetLevelCommand;
+import frc.team2767.deepspace.command.vision.LightsOnCommand;
+import frc.team2767.deepspace.subsystem.Action;
+import frc.team2767.deepspace.subsystem.ElevatorLevel;
+import frc.team2767.deepspace.subsystem.ElevatorSubsystem;
+import frc.team2767.deepspace.subsystem.GamePiece;
+
+public class ControlledHatchPlaceCommandGroup extends CommandGroup {
+
+  public ControlledHatchPlaceCommandGroup() {
+
+    addSequential(new LogCommand("BEGIN AUTO HATCH PLACE"));
+
+    addSequential(new LightsOnCommand());
+    addSequential(
+        new CommandGroup() {
+          {
+            addParallel(new SetActionCommand(Action.PLACE));
+            addParallel(new SetGamePieceCommand(GamePiece.HATCH));
+            addParallel(new SetLevelCommand(ElevatorLevel.ONE));
+          }
+        });
+    addSequential(new ElevatorSetPositionCommand(ElevatorSubsystem.kHatchLowPositionInches));
+    addSequential(new BiscuitPositionAboveCameraCommand());
+    addSequential(new ControlledAutoAlignPlaceCommand());
+    addSequential(new BiscuitExecutePlanCommand());
+    addSequential(new HoldHeadingUntilCompressionCommand());
+    addSequential(new LogCommand("END HATCH PLACE"));
+  }
+}
