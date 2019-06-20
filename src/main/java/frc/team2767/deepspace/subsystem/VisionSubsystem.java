c208aca38cf47c3cce5d6ddbbe9319361cd5aa9c
Roberts Kalnins <rkalnins314@gmail.com>
2019-06-20 17:06:09 -0400

    Add PS camera calibration (right side)

--- src/main/java/frc/team2767/deepspace/subsystem/VisionSubsystem.java
+++ src/main/java/frc/team2767/deepspace/subsystem/VisionSubsystem.java
@@ -29,18 +29,18 @@ public class VisionSubsystem extends Subsystem implements Item {
   private static final double CAMERA_X = 3.5;
   private static final double CAMERA_Y_LEFT = -13.5;
   private static final double CAMERA_Y_RIGHT = 13.5;
-  private static final double GLUE_CORRECTION_FACTOR_RIGHT = 0.2097; // -0.9536 comp
-  private static final double GLUE_CORRECTION_FACTOR_LEFT = -0.4893; // -1.25 comp
+  private static final double GLUE_CORRECTION_FACTOR_RIGHT = -1.925; // -0.9536 comp
+  private static final double GLUE_CORRECTION_FACTOR_LEFT = 0.0; // -1.25 comp
   private static final double CAMERA_DEGREES_PER_PIXEL_ADJUSTMENT_RIGHT =
       1.0; // 1.0 is zero value 0.85
   private static final double CAMERA_DEGREES_PER_PIXEL_ADJUSTMENT_LEFT =
       1.0; // 1.0 is zero value 0.85
   private static final double CAMERA_POSITION_BEARING_LEFT = -90.0;
   private static final double CAMERA_POSITION_BEARING_RIGHT = 90.0;
-  private static final double CAMERA_RANGE_SLOPE_RIGHT = 1.0241; // 1.2449
-  private static final double CAMERA_RANGE_OFFSET_RIGHT = -4.4373; // -4.3949
-  private static final double CAMERA_RANGE_SLOPE_LEFT = 1.0384; // 0.8259
-  private static final double CAMERA_RANGE_OFFSET_LEFT = -4.5175; // -5.6325
+  private static final double CAMERA_RANGE_SLOPE_RIGHT = 0.82713; // 1.2449
+  private static final double CAMERA_RANGE_OFFSET_RIGHT = 7.687; // -4.3949
+  private static final double CAMERA_RANGE_SLOPE_LEFT = 0.0; // 0.8259
+  private static final double CAMERA_RANGE_OFFSET_LEFT = 0.0; // -5.6325
   // NEGATIVE = TOWARDS FIELD LEFT
   private static final double STRAFE_CORRECTION_RIGHT =
       -0.925; // -1.0 // NEGATIVE TO FIELD LEFT FOR THIS ONE?
